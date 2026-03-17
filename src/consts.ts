bc4c832877933982d2bb5fcf49d92fb5a22cabd1
Ikaros <62824022+Ikaros-Synapus@users.noreply.github.com>
2026-03-17 12:31:18 +0800

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -9,8 +9,8 @@ export const BLOG_SLUG_KEY = process.env.BLOG_SLUG_KEY || ''
  */
 export const GITHUB_CONFIG = {
 	OWNER: process.env.NEXT_PUBLIC_GITHUB_OWNER || 'Ikaros-Synapus',
-	REPO: process.env.NEXT_PUBLIC_GITHUB_REPO || '2025-blog-public',
+	REPO: process.env.NEXT_PUBLIC_GITHUB_REPO || 'synapusblog',
 	BRANCH: process.env.NEXT_PUBLIC_GITHUB_BRANCH || 'main',
-	APP_ID: process.env.NEXT_PUBLIC_GITHUB_APP_ID || '-',
+	APP_ID: process.env.NEXT_PUBLIC_GITHUB_APP_ID || '3111798',
 	ENCRYPT_KEY: process.env.NEXT_PUBLIC_GITHUB_ENCRYPT_KEY || 'wudishiduomejimo',
 } as const
